4d624b8fb73300db8001745f83eaea0fe815cfd0
cmallen2106 <cmallen2106@gmail.com>
2021-07-28 19:38:42 -0600

    Choice menu with loop

--- Assignment2/src/Figures.java
+++ Assignment2/src/Figures.java
@@ -13,6 +13,8 @@ public class Figures {
 		Scanner scan = new Scanner (System.in); 
 		
 		int size; 
+		int menuSelect; 
+		boolean hasNotQuit = true; 
 		
 		
 		//Ask user for size of figure
@@ -21,11 +23,52 @@ public class Figures {
 		
 		//If the user enters a number that is not odd, ask him/her to reenter the number until it is odd.
 		while (size % 2 ==0) {
-			System.out.println("Invalid - must be off number");
-			System.out.println("Enter size of figure: "); 
+			System.out.println("Invalid figure size - must be an odd number");
+			System.out.println("Renter the size of teh figure: "); 
 			size = scan.nextInt(); 
 		}
 		
+		//Enter a loop in which you will display a menu of choices (1-4) and wait for user input
+		while (hasNotQuit == true) {
+			printMenu(); 
+			menuSelect = scan.nextInt(); 
+			System.out.println();
+			
+			//printBox
+			if (menuSelect == 1) {
+				printBox(size); 
+			}
+			
+			//printDiamond
+			if (menuSelect == 2) {
+				printDiamond(size); 
+			}
+			
+			//printX
+			if (menuSelect == 3) {
+				printX(size); 
+			}
+			
+			//Quit Program
+			if (menuSelect == 4) {
+				hasNotQuit = false; 
+				System.out.println("Good bye!");
+			}
+		}
+		
+		//printMenu method
+		private static void printMenu() {
+			System.out.println("MENU");
+			System.out.println("1. Print box");
+			System.out.println("2. Print diamond"); 
+			System.out.println("3. Print X"); 
+			System.out.println("4. Quit program"); 
+			System.out.println("Please select an option: "); 
+		}
+		
 	}
+	
+	//Close scanner
+	scan.close(); 
 
 }
